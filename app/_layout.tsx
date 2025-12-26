103d190429423823122163e459d33f9e7137aa6d
Jdexurbano <dexterurbano25@gmail.com>
2025-12-26 16:14:29 +0800

    load and create font files

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,30 @@
+import {
+  Quicksand_300Light,
+  Quicksand_400Regular,
+  Quicksand_500Medium,
+  Quicksand_600SemiBold,
+  Quicksand_700Bold,
+} from "@expo-google-fonts/quicksand";
+import { useFonts } from "expo-font";
 import { Stack } from "expo-router";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function RootLayout() {
-  return <Stack />;
+  //load the font to make it global
+  const [fontsLoaded] = useFonts({
+    Quicksand_300Light,
+    Quicksand_400Regular,
+    Quicksand_500Medium,
+    Quicksand_600SemiBold,
+    Quicksand_700Bold,
+  });
+
+  //Prevent rendering screens until fonts are ready
+  if (!fontsLoaded) return null;
+
+  return (
+    <SafeAreaView style={{ flex: 1 }}>
+      <Stack screenOptions={{ headerShown: false }} />
+    </SafeAreaView>
+  );
 }
---
+++ assets/fonts/fonts.ts
@@ -0,0 +1,7 @@
+export const fonts = {
+  light: "Quicksand_300Light",
+  regular: "Quicksand_400Regular",
+  medium: "Quicksand_500Mediumt",
+  semibold: "Quicksand_600SemiBold",
+  bold: "Quicksand_700Bold",
+};
